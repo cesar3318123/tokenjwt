be009198cbfaa27c01d9b85675cbd87f924c65e9
cesar3318123 <www.cesar3318123@gmail.com>
2025-02-22 16:23:43 -0600

    Ya esta

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,11 @@
 const API_URL = "https://vrn0rzfj-3000.usw3.devtunnels.ms/";
 
 //Manejo del registro de usuario
-async function registrarUsuario(event) {
+async function iniciarSesion(event) {
+
+    window.location.href = "perfil.html"
+/*
+    
     event.preventDefault();
 
     //Obtiene los valores ingresados en el campo de registro
@@ -10,11 +14,61 @@ async function registrarUsuario(event) {
     const password =  document.getElementById("contrasena").value;
 
     //Enviar la solicitu POST al backend con los datos del usuarios
-    const response = await fetch(`${API_URL}/registro`, {
+    const response = await fetch(`${API_URL}/login`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ username, password })
     })
+
+    const data = await response.json(); // Convierte la respuesta en json
+
+    if(response.ok) {
+        localStorage.setItem("token, data.token");
+        alert("Inicio de sesión exitoso");
+        window.location.href = "perfil.html"
+    } else {
+
+        alert("Error: " + data.message)
+
+    }*/
+
+
 }
 
-const data = await response.json(); // Convierte la respuesta en json
+function verificarAutenticacion() {
+    const token = localStorage.getItem("token");
+    if(!token){
+        alert("No tienes acceso");
+        window.location.href="login.html";
+    }
+}
+
+function cerrarSesion() {
+    alert("Funciona bien")
+    /*
+    localStorage.removeItem("token")
+    alert("Sesión cerrada");
+    window.location.href = "login.html";
+    */
+
+}
+
+function registrarUsuario() {
+    alert("Perfecto")
+}
+
+if(window.location.pathname.includes("perfil.html")) {
+    verificarAutenticacion();
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const loginBtn = document.getElementById("login-btn");
+    const registroForm = document.getElementById("registro-form");
+    const logoutBtn = document.getElementById("logout-btn");
+
+    if (loginBtn) loginBtn.addEventListener("click", iniciarSesion);
+    if (registroForm) registroForm.addEventListener("submit", registrarUsuario);
+    if (logoutBtn) logoutBtn.addEventListener("click", cerrarSesion);
+
+})
